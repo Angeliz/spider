55b471397f84426166d3467d113ca0a252040de9
Angeliz <471330418@qq.com>
2018-03-15 00:02:19 +0800

    add

--- nodejs/poet-spider.js
+++ nodejs/poet-spider.js
@@ -25,11 +25,25 @@ function startRequest(url) {
         // 监听end事件，若整个HTML页面获取完毕，就执行回调函数
         res.on('end',function () {
             // console.log(html);
-            let $ = cheerio.load(html);
-            let imgSrc=$('#mmComponent_images_1').first();
-            // let imgSrc=$('ul .dgControl_list').attr('class');
-            console.log(imgSrc);
+            console.log(html.length);
+            var $ = cheerio.load(html);
+            console.log('load ok');
+            // var imgSrc=$('#imgid').children().first().children().last().attr('class');
+            var list=$('#imgid').children().length;
+            // var $=$(list);
+            // var imgSrc=$('img.main_img');
+            // var imgSrc=10;
+            // list.each(function (index,item) {
+            //     if(index===0){
+            //         imgSrc=$('ul.imglist',this).length;
+            //     }
+            // });
+            // // let imgSrc=$('ul .dgControl_list').attr('class');
+            // console.log(123);
+            console.log(list);
         });
+    }).on('error',function (err) {
+        console.log(err);
     });
 }
-startRequest('https://cn.bing.com/images/search?q=诗人杜甫');
+startRequest('https://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&sf=1&fmq=&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&fm=index&pos=history&word=诗人杜甫');
